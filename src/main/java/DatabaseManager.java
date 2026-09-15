f6d8a6060b8a160224a371705b5d64ab4677c0df
Tattarush <tattarush@gmail.com>
2026-09-15 23:54:16 +0300

    добавлена база H2 костяк

---
+++ src/main/java/DatabaseManager.java
@@ -0,0 +1,34 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DatabaseManager {
+
+    private static final String DB_URL = "jdbc:h2:./db/botdata";
+    private static final String USER = "sa";
+    private static final String PASS = "";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(DB_URL, USER, PASS);
+    }
+
+    public static void initDatabase() {
+
+        String createTAbleSQL = "CREATE TABLE IF NOT EXISTS user_notes(" +
+                "user_id BIGINT PRIMARY KEY, " +
+                "note_text TEXT " +
+                ")";
+        try (Connection conn = getConnection();
+        Statement stmt = conn.createStatement()){
+            stmt.execute(createTAbleSQL);
+            System.out.println("База данных успешно инициализирована");
+        } catch (SQLException e) {
+            System.out.println("ошибка при инициализации базы данных!");
+            e.printStackTrace();
+        }
+
+
+
+    }
+}
