e84e1e2557b9eccceaa6a8c0d0a3d3ed58d4cf69
Tattarush <tattarush@gmail.com>
2026-09-19 16:22:04 +0300

    Добавлен обработчик событий по запросам из БД за разные периоды

--- src/main/java/com/bot/BotState.java
+++ src/main/java/com/bot/BotState.java
@@ -7,5 +7,5 @@ public enum BotState {
 
     QUERY_MENU,  // ожидание выбора из вариантов запроса, все, по датам, конкретная дата
     AWAITING_PERIOD_INPUT, //Ожидание ввода периода дат
-    AWAITING_SINGLE_DATE //жидание ввода конкретной даты
+    AWAITING_SINGLE_DATE //ожидание ввода конкретной даты
 }
